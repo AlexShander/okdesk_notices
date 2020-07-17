a00064a7916e6a41f0c71dee5627f86809a324e2
a.shander <a.shander@otgroup.kz>
2020-07-17 15:08:31 +0600

    Long waiting will send only once at ~9 a.m.

--- overdue_chk/main.py
+++ overdue_chk/main.py
@@ -43,7 +43,7 @@ def get_list_long_wait_issues():
     answered_24h_waiting_issues_url = u"http://{}.okdesk.ru/api/v1/issues/count?api_token=\
 {}&status[]=waiting&updated_since={}".format(okdesk_api_credintails.get('domain'),
                                              okdesk_api_credintails.get('api_token'),
-                                             datetime.strftime(datetime.now() - timedelta(1), '%d-%m-%Y'))
+                                             datetime.strftime(datetime.now() - timedelta(days=2), '%d-%m-%Y'))
     list_waiting_issues = get_list_issues(waiting_issues_url)
     list_answered_24h_issues = get_list_issues(answered_24h_waiting_issues_url)
     return list_waiting_issues.difference(list_answered_24h_issues)
@@ -60,7 +60,7 @@ def main():
     try:
         redis_okdesk = redis.Redis(host='redis_okdesk', port=6379, db=0)
         while True:
-            while 9 > time.localtime()[3] or time.localtime()[3]  > 17:
+            while 9 > time.localtime()[3] or time.localtime()[3] > 17:
                 time.sleep(360)
             overdue_reaction_issue_notices = get_list_overdue_reaction_issues()
             for notice in overdue_reaction_issue_notices:
@@ -71,14 +71,15 @@ def main():
                     if redis_okdesk.publish('overdue_reaction_noticed_email', u"{}_email".format(str(notice))) > 0:
                         redis_okdesk.set(u"{}_email".format(str(notice)), 'issue', ex=60 * 60 * 12)
             long_waiting_issues_notices = get_list_long_wait_issues()
-            for notice in long_waiting_issues_notices:
-                if redis_okdesk.get(u"{}_tlgrm".format(str(notice))) is None:
-                    if redis_okdesk.publish('long_wait_noticed_tlgrm', u"{}_tlgrm".format(str(notice))) > 0:
-                        redis_okdesk.set(u"{}_tlgrm".format(str(notice)), 'issue', ex=60 * 60 * 12)
-                if redis_okdesk.get(u"{}_email".format(str(notice))) is None:
-                    if redis_okdesk.publish('long_wait_noticed_email', u"{}_email".format(str(notice))) > 0:
-                        redis_okdesk.set(u"{}_email".format(str(notice)), 'issue', ex=60 * 60 * 12)
-            overdue_issue_notices = get_list_overdue_execution_issues()
+            if time.localtime()[3] == 9 and 0 <= time.localtime()[4] <= 6:
+                for notice in long_waiting_issues_notices:
+                    if redis_okdesk.get(u"{}_tlgrm".format(str(notice))) is None:
+                        if redis_okdesk.publish('long_wait_noticed_tlgrm', u"{}_tlgrm".format(str(notice))) > 0:
+                            redis_okdesk.set(u"{}_tlgrm".format(str(notice)), 'issue', ex=60 * 60 * 12)
+                    if redis_okdesk.get(u"{}_email".format(str(notice))) is None:
+                        if redis_okdesk.publish('long_wait_noticed_email', u"{}_email".format(str(notice))) > 0:
+                            redis_okdesk.set(u"{}_email".format(str(notice)), 'issue', ex=60 * 60 * 12)
+                overdue_issue_notices = get_list_overdue_execution_issues()
             for notice in overdue_issue_notices:
                 if redis_okdesk.get(u"{}_tlgrm".format(str(notice))) is None:
                     if redis_okdesk.publish('overdue_noticed_tlgrm', u"{}_tlgrm".format(str(notice))) > 0:
